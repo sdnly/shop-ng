a610023408b09dbd8232c24511b2fe405f1636e4
sdnly <nesentic@gmail.com>
2021-05-13 20:13:47 +0200

    hm

---
+++ src/app/shopping-cart/cart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ProductCart } from './cart';
+import { Product } from 'src/app/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+      private shoppingCartUrl = 'http://localhost:8080/cart'
+
+  constructor(private http: HttpClient) {
+   this.shoppingCartUrl = 'http://localhost:8080/cart'}
+
+   public getCart(): Observable<ProductCart[]> {
+   return this.http.get<ProductCart[]>(this.shoppingCartUrl)
+   }
+
+   public addToCart(product: Product): Observable<ProductCart> {
+   return this.http.post<ProductCart>(this.shoppingCartUrl, product)
+   }
+}
